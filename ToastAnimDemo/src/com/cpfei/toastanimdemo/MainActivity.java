b273724ca136bfc72612267dcc2990cceefb7d42
cpfei <847626908gh>
2016-08-24 14:10:34 +0800

    GridView Line

--- ToastAnimDemo/src/com/cpfei/toastanimdemo/MainActivity.java
+++ ToastAnimDemo/src/com/cpfei/toastanimdemo/MainActivity.java
@@ -1,8 +1,13 @@
 package com.cpfei.toastanimdemo;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
 
 public class MainActivity extends Activity {
 
@@ -10,7 +15,42 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		GridView gr = (GridView) findViewById(R.id.gridView);
+		gr.setAdapter(new MyAdapter());
 	}
+	
+	
+	class MyAdapter extends BaseAdapter{
+
+		@Override
+		public int getCount() {
+			return 5;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return null;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return 0;
+		}
+
+		@SuppressLint("ViewHolder")
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			
+			View view  = LayoutInflater.from(MainActivity.this).inflate(R.layout.gridview_item, null);
+			
+			return view;
+		}
+		
+	}
+	
+	
+	
 
 	public void showToast(View v) {
 
